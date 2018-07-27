a24fa5466ce7527f440a6cc60e21e77949edcc3c
anne <richardson.ae@gmail.com>
2018-07-27 15:20:30 -0500

    Removes default REST comments

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,28 +1,20 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
 
-  # GET /cars
-  # GET /cars.json
   def index
     @cars = Car.ordered
   end
 
-  # GET /cars/1
-  # GET /cars/1.json
   def show
   end
 
-  # GET /cars/new
   def new
     @car = Car.new
   end
 
-  # GET /cars/1/edit
   def edit
   end
 
-  # POST /cars
-  # POST /cars.json
   def create
     @car = Car.new(car_params)
 
@@ -37,10 +29,8 @@ class CarsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cars/1
-  # PATCH/PUT /cars/1.json
   def update
-    @car_with_broadcast = CarWithBroadcast.new(@car, car_params) 
+    @car_with_broadcast = CarWithBroadcast.new(@car, car_params)
 
     respond_to do |format|
       if @car_with_broadcast.save
@@ -53,8 +43,6 @@ class CarsController < ApplicationController
     end
   end
 
-  # DELETE /cars/1
-  # DELETE /cars/1.json
   def destroy
     @car.destroy
     respond_to do |format|
